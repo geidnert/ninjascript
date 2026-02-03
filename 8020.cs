0d1a61feae8e27491839e08394b8351723dc4241
andreas.geidnert <andreas.geidnert@king.com>
2026-02-03 11:35:45 +0100

    added session and skip times

--- 8020.cs
+++ 8020.cs
@@ -6,7 +6,12 @@ using System;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Windows.Media;
+using System.Xml.Serialization;
+using NinjaTrader.Gui;
 using NinjaTrader.Cbi;
+using NinjaTrader.Gui.Tools;
+using NinjaTrader.NinjaScript;
+using NinjaTrader.NinjaScript.DrawingTools;
 #endregion
 
 namespace NinjaTrader.NinjaScript.Strategies
@@ -32,6 +37,8 @@ namespace NinjaTrader.NinjaScript.Strategies
 		private bool settingsLogged;
 		private int tradesInRange;
 		private bool tradeCompletedSinceLastTrigger;
+		private bool sessionClosed;
+		private bool inSkipWindow;
 
 		protected override void OnStateChange()
 		{
@@ -47,10 +54,21 @@ namespace NinjaTrader.NinjaScript.Strategies
 				ExitOnSessionCloseSeconds = 30;
 				BarsRequiredToTrade = 1;
 
-				TriggerValue = 40;
+				TriggerValue = 30;
 				TakeProfitPoints = 10;
 				StopLossPoints = 10;
 				MaxTradesPerRange = 1;
+
+				SessionStart = new TimeSpan(9, 30, 0);
+				SessionEnd = new TimeSpan(16, 0, 0);
+				NoTradesAfter = new TimeSpan(15, 30, 0);
+				SkipStart = TimeSpan.Zero;
+				SkipEnd = TimeSpan.Zero;
+				CloseAtSessionEnd = true;
+				ForceCloseAtSkipStart = true;
+				SessionBrush = Brushes.Gold;
+				SkipBrush = Brushes.DarkSlateGray;
+				NoTradesAfterBrush = Brushes.Red;
 			}
 			else if (State == State.Configure)
 			{
@@ -67,15 +85,19 @@ namespace NinjaTrader.NinjaScript.Strategies
 				lastOrdersPlacedLogBase = int.MinValue;
 				tradesInRange = 0;
 				tradeCompletedSinceLastTrigger = false;
+				sessionClosed = false;
+				inSkipWindow = false;
 				LogDebug($"Init: EntryHandling={EntryHandling} StopTargetHandling={StopTargetHandling}");
 			}
 		}
 
 		protected override void OnBarUpdate()
 		{
-			if (State == State.Historical)
+			if (BarsInProgress == 0)
+			{
+				DrawSessionAndSkip();
 				return;
-
+			}
 			if (BarsInProgress != 1)
 				return;
 
@@ -90,6 +112,46 @@ namespace NinjaTrader.NinjaScript.Strategies
 				LogDebug($"Runtime settings: EntryHandling={EntryHandling} EntriesPerDirection={EntriesPerDirection} StopTargetHandling={StopTargetHandling} IsUnmanaged={IsUnmanaged}");
 				settingsLogged = true;
 			}
+
+			if (!TimeInSession(Time[0]))
+				return;
+
+			bool nowInSkip = TimeInSkip(Time[0]);
+			if (nowInSkip && !inSkipWindow)
+			{
+				inSkipWindow = true;
+				LogDebug("Entered skip window.");
+				if (ForceCloseAtSkipStart)
+					FlattenAll("SkipWindow");
+			}
+			else if (!nowInSkip && inSkipWindow)
+			{
+				inSkipWindow = false;
+				LogDebug("Exited skip window.");
+			}
+
+			if (nowInSkip)
+				return;
+
+			if (CrossedSessionEnd(Time[1], Time[0]) && !sessionClosed)
+			{
+				sessionClosed = true;
+				LogDebug("Session end crossed.");
+				if (CloseAtSessionEnd)
+					FlattenAll("SessionEnd");
+				return;
+			}
+			if (TimeInSession(Time[0]) && sessionClosed)
+				sessionClosed = false;
+
+			if (CrossedNoTradesAfter(Time[1], Time[0]))
+			{
+				LogDebug("NoTradesAfter crossed. Canceling entry orders.");
+				CancelEntryOrders();
+			}
+
+			if (TimeInNoTradesAfter(Time[0]))
+				return;
 
 			double lastPrice = Closes[1][0];
 			double prevPrice = Closes[1][1];
@@ -239,6 +301,28 @@ namespace NinjaTrader.NinjaScript.Strategies
 			CancelIfWorking(shortTargetOrder);
 		}
 
+		private void CancelEntryOrders()
+		{
+			CancelIfWorking(longEntryOrder);
+			CancelIfWorking(shortEntryOrder);
+		}
+
+		private void FlattenAll(string reason)
+		{
+			CancelAllWorkingOrders();
+
+			if (Position.MarketPosition == MarketPosition.Long && Position.Quantity > 0)
+			{
+				SubmitOrderUnmanaged(1, OrderAction.Sell, OrderType.Market, Position.Quantity, 0, 0, null, $"FlattenLong_{reason}");
+				LogDebug($"Flatten long due to {reason}.");
+			}
+			else if (Position.MarketPosition == MarketPosition.Short && Position.Quantity > 0)
+			{
+				SubmitOrderUnmanaged(1, OrderAction.BuyToCover, OrderType.Market, Position.Quantity, 0, 0, null, $"FlattenShort_{reason}");
+				LogDebug($"Flatten short due to {reason}.");
+			}
+		}
+
 		private void ResetForNewBase(int baseKey)
 		{
 			if (baseKey == activeBase)
@@ -267,6 +351,103 @@ namespace NinjaTrader.NinjaScript.Strategies
 			return order != null && (order.OrderState == OrderState.Working || order.OrderState == OrderState.Accepted);
 		}
 
+		private bool TimeInSession(DateTime time)
+		{
+			if (SessionStart == SessionEnd)
+				return true;
+			return IsTimeInRange(time.TimeOfDay, SessionStart, SessionEnd);
+		}
+
+		private bool TimeInSkip(DateTime time)
+		{
+			if (SkipStart == SkipEnd)
+				return false;
+			return IsTimeInRange(time.TimeOfDay, SkipStart, SkipEnd);
+		}
+
+		private bool TimeInNoTradesAfter(DateTime time)
+		{
+			if (NoTradesAfter == SessionEnd)
+				return false;
+
+			TimeSpan now = time.TimeOfDay;
+			if (SessionStart < SessionEnd)
+				return now >= NoTradesAfter && now < SessionEnd;
+			return now >= NoTradesAfter || now < SessionEnd;
+		}
+
+		private bool CrossedSessionEnd(DateTime prev, DateTime now)
+		{
+			return IsTimeInRange(prev.TimeOfDay, SessionStart, SessionEnd)
+			       && !IsTimeInRange(now.TimeOfDay, SessionStart, SessionEnd);
+		}
+
+		private bool CrossedNoTradesAfter(DateTime prev, DateTime now)
+		{
+			return !TimeInNoTradesAfter(prev) && TimeInNoTradesAfter(now);
+		}
+
+		private bool IsTimeInRange(TimeSpan now, TimeSpan start, TimeSpan end)
+		{
+			if (start < end)
+				return now >= start && now < end;
+			return now >= start || now < end;
+		}
+
+		private void DrawSessionAndSkip()
+		{
+			if (CurrentBar < 1)
+				return;
+
+			if (SessionStart != SessionEnd)
+			{
+				DateTime barTime = Time[0];
+				DateTime sessionStartTime = barTime.Date + SessionStart;
+				DateTime sessionEndTime = barTime.Date + SessionEnd;
+				if (SessionStart > SessionEnd)
+					sessionEndTime = sessionEndTime.AddDays(1);
+
+				string sessionTag = $"8020_SessionFill_{sessionStartTime:yyyyMMdd_HHmm}";
+				if (DrawObjects[sessionTag] == null)
+				{
+					Draw.Rectangle(this, sessionTag, false,
+						sessionStartTime, 0,
+						sessionEndTime, 30000,
+						Brushes.Transparent, SessionBrush ?? Brushes.Gold, 20).ZOrder = -1;
+				}
+
+				if (NoTradesAfter != SessionEnd)
+				{
+					DateTime noTradesAfterTime = barTime.Date + NoTradesAfter;
+					if (SessionStart > SessionEnd && NoTradesAfter < SessionStart)
+						noTradesAfterTime = noTradesAfterTime.AddDays(1);
+					Draw.VerticalLine(this, $"8020_NoTradesAfter_{sessionStartTime:yyyyMMdd_HHmm}", noTradesAfterTime,
+						NoTradesAfterBrush ?? Brushes.Red, DashStyleHelper.Dash, 2);
+				}
+			}
+
+			if (SkipStart != SkipEnd)
+				DrawSkipWindow(SkipStart, SkipEnd);
+		}
+
+		private void DrawSkipWindow(TimeSpan start, TimeSpan end)
+		{
+			DateTime barTime = Time[0];
+			DateTime windowStart = barTime.Date + start;
+			DateTime windowEnd = barTime.Date + end;
+			if (start > end)
+				windowEnd = windowEnd.AddDays(1);
+
+			string tag = $"8020_Skip_{windowStart:yyyyMMdd_HHmm}";
+			if (DrawObjects[tag] == null)
+			{
+				Draw.Rectangle(this, tag, false,
+					windowStart, 0,
+					windowEnd, 30000,
+					Brushes.Transparent, SkipBrush ?? Brushes.DarkSlateGray, 25).ZOrder = -1;
+			}
+		}
+
 		private void LogDebug(string message, DateTime? time = null)
 		{
 			if (!DebugEnabled)
@@ -292,6 +473,67 @@ namespace NinjaTrader.NinjaScript.Strategies
 		[Range(1, int.MaxValue), NinjaScriptProperty]
 		[Display(ResourceType = typeof(Custom.Resource), Name = "MaxTradesPerRange", GroupName = "NinjaScriptParameters", Order = 3)]
 		public int MaxTradesPerRange { get; set; }
+
+		[NinjaScriptProperty]
+		[Display(ResourceType = typeof(Custom.Resource), Name = "SessionStart", GroupName = "Session", Order = 0)]
+		public TimeSpan SessionStart { get; set; }
+
+		[NinjaScriptProperty]
+		[Display(ResourceType = typeof(Custom.Resource), Name = "SessionEnd", GroupName = "Session", Order = 1)]
+		public TimeSpan SessionEnd { get; set; }
+
+		[NinjaScriptProperty]
+		[Display(ResourceType = typeof(Custom.Resource), Name = "NoTradesAfter", GroupName = "Session", Order = 2)]
+		public TimeSpan NoTradesAfter { get; set; }
+
+		[NinjaScriptProperty]
+		[Display(ResourceType = typeof(Custom.Resource), Name = "SkipStart", GroupName = "Session", Order = 3)]
+		public TimeSpan SkipStart { get; set; }
+
+		[NinjaScriptProperty]
+		[Display(ResourceType = typeof(Custom.Resource), Name = "SkipEnd", GroupName = "Session", Order = 4)]
+		public TimeSpan SkipEnd { get; set; }
+
+		[NinjaScriptProperty]
+		[Display(ResourceType = typeof(Custom.Resource), Name = "CloseAtSessionEnd", GroupName = "Session", Order = 5)]
+		public bool CloseAtSessionEnd { get; set; }
+
+		[NinjaScriptProperty]
+		[Display(ResourceType = typeof(Custom.Resource), Name = "ForceCloseAtSkipStart", GroupName = "Session", Order = 6)]
+		public bool ForceCloseAtSkipStart { get; set; }
+
+		[XmlIgnore]
+		[Display(ResourceType = typeof(Custom.Resource), Name = "SessionBrush", GroupName = "Session", Order = 7)]
+		public Brush SessionBrush { get; set; }
+
+		[Browsable(false)]
+		public string SessionBrushSerialize
+		{
+			get { return Serialize.BrushToString(SessionBrush); }
+			set { SessionBrush = Serialize.StringToBrush(value); }
+		}
+
+		[XmlIgnore]
+		[Display(ResourceType = typeof(Custom.Resource), Name = "SkipBrush", GroupName = "Session", Order = 8)]
+		public Brush SkipBrush { get; set; }
+
+		[Browsable(false)]
+		public string SkipBrushSerialize
+		{
+			get { return Serialize.BrushToString(SkipBrush); }
+			set { SkipBrush = Serialize.StringToBrush(value); }
+		}
+
+		[XmlIgnore]
+		[Display(ResourceType = typeof(Custom.Resource), Name = "NoTradesAfterBrush", GroupName = "Session", Order = 9)]
+		public Brush NoTradesAfterBrush { get; set; }
+
+		[Browsable(false)]
+		public string NoTradesAfterBrushSerialize
+		{
+			get { return Serialize.BrushToString(NoTradesAfterBrush); }
+			set { NoTradesAfterBrush = Serialize.StringToBrush(value); }
+		}
 
 		[NinjaScriptProperty]
 		[Display(ResourceType = typeof(Custom.Resource), Name = "DebugEnabled", GroupName = "NinjaScriptParameters", Order = 4)]
